48f0ac49222eab2c33c36d0b3f5cca1ee3077cb5
Manuismael <emmanueliyanda632@gmail.com>
2024-07-12 11:27:48 +0100

    first version

---
+++ public/home.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="../static/css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Amber</title>
+  </head>
+  <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-0">
+                <h1 class="navbar-brand font-weight-bold" href="#!">Amber</h1>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Acheter</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Assitance</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Inscription</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Connexion</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container px-0 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-5">
+                    <h1 class="font-weight-bold">Amber,</h1>
+                    <h2>Votre plateforme d'échanges de monnaies.</h2>
+                    
+                </div>
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg" alt="..." /></div>
+            </div>
+        </div>
+
+        <div class="container px-0">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">Monnaie</th>
+                    <th scope="col">Valeur</th>
+                    <th scope="col">Taux</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>Bitcoin(BTC)</td>
+                    <td>6215.00$</td>
+                    <td>+1.5</td>
+                    </tr>
+                    <tr>
+                    <td>Ethereum(ETH)</td>
+                    <td>4201.33$</td>
+                    <td>+0.1</td>
+                    </tr>
+                    <tr>
+                    <td>Solana(SOL)</td>
+                    <td>2021.11$</td>
+                    <td>-0.2</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="container px-4 px-lg-2">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-5">
+                    <h2 class="font-weight-bold">Inscrivez-vous,et effectuez des transactions illimités</h2>
+                    <a class="btn btn-dark" href="#!">S'inscrire</a>
+                </div>
+                <div class="col-lg-7 col-md-6 d-none d-md-block"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg" alt="..." /></div>
+            </div>
+        </div>
+
+        <!-- Footer-->
+        <footer class="py-5 bg-dark text-light px-4 px-lg">
+            <div class="container text-light px-4 px-lg-5">
+                <p class="m-0 text-center text-white">Abonnez-vous à notre Newsletter</p>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control w-25" placeholder="E-mail" aria-label="E-mail" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">S'abonner</button>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-0">
+                <div class="card-deck ">
+                        <div class="card border-0 bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title">A Propos</h5>
+                                <p class="card-text"><a href="#" class="">FAQ</a></p>
+                                <p class="card-text"><a href="#" class="">Contactez-nous</a></p>
+                                <p class="card-text"><a href="#" class="">Qui sommes-nous ?</a></p>
+                            </div>
+                        </div>
+                        <div class="card border-0  bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title">Assistances Techniques</h5>
+                                <p class="card-text"><a href="#" class="">FAQ</a></p>
+                                <p class="card-text"><a href="#" class="">Contactez-nous</a></p>
+                                <p class="card-text"><a href="#" class="">Qui sommes-nous ?</a></p>
+                            </div>
+                        </div>
+                        <div class="card border-0  bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title">Mentions Légales</h5>
+                                <p class="card-text"><a href="#" class="">FAQ</a></p>
+                                <p class="card-text"><a href="#" class="">Contactez-nous</a></p>
+                                <p class="card-text"><a href="#" class="">Qui sommes-nous ?</a></p>
+                            </div>
+                        </div>
+                        <div class="card border-0  bg-dark">
+                            <div class="card-body">
+                                <h5 class="card-title">Social</h5>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            
+        </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
